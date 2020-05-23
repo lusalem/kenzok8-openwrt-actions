62b932ca98bf421135f7d46f45d0207d2c542055
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-05-23 19:44:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,13 +8,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/litte.git package/litte
-rm -rf package/lean/luci-theme-argon
-rm -rf package/litte/luci-app-passwall && rm -rf package/litte/lluci-app-ssr-plus
+rm -rf package/litte/luci-app-passwall && rm -rf package/lean/lluci-app-ssr-plus
 rm -rf package/litte/microsocks && rm -rf package/litte/redsocks2 && rm -rf package/litte/tcpping
-rm -rf package/openwrt-package/lienol/luci-theme-bootstrap-mod
-sed -i 's/LuCI Master/LuCI master by kenzo/g' /usr/lib/lua/luci/version.lua
-sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
-echo 'luciversion = "19.07"' >> /usr/lib/lua/luci/version.lua
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+date=`date +%m.%d.%Y`
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by kenzo'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
+cp -f default-settings package/*/*/default-settings/files/zzz-default-settings
 ./scripts/feeds update -a
 ./scripts/feeds install

76960543963a5a460fbbc67a17fd47f7eccc8c53
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-05-25 15:38:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,13 +5,19 @@
 # Author: kenzo
 # https://github.com/kenzok8
 #=================================================
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/kenzok8/litte.git package/litte
-rm -rf package/litte/luci-app-passwall && rm -rf package/lean/luci-app-ssr-plus
+取消注释提供源
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+添加自定义源
+sed -i '$a src-git litte https://github.com/kenzok8/litte.git' feeds.conf.default
+rm -rf package/lienol/luci-app-passwall && rm -rf package/lean/luci-app-ssr-plus && rm -rf package/litte/luci-app-ssr-plus
 rm -rf package/litte/microsocks && rm -rf package/litte/redsocks2 && rm -rf package/litte/tcpping
+rm -rf package/litte/adguardhome && rm -rf package/litte/luci-app-adguardhome
+rm -rf package/litte/luci-app-advancedsetting && rm -rf package/litte/luci-app-aliddns && rm -rf package/litte/luci-app-clash
+rm -rf package/litte/gost && rm -rf package/litte/luci-app-gost && rm -rf package/litte/luci-app-eqos
+rm -rf package/ctcgfw/luci-app-openclash && rm -rf package/litte/luci-app-smartdns && rm -rf package/litte/smartdns
+rm -rf package/ctcgfw/luci-theme-atmaterial && rm -rf package/ctcgfw/luci-theme-opentomato && rm -rf package/ctcgfw/luci-theme-opentomcat
 date=`date +%m.%d.%Y`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by kenzo'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D by kenzo'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
 cp -f default-settings package/*/*/default-settings/files/zzz-default-settings
 ./scripts/feeds update -a
